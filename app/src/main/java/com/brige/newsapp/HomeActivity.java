d89594d9d3ee747eeda58cea610ff1ebfed12f94
Brian <brigeveriz7@gmail.com>
2022-03-21 10:17:14 +0300

    Created new video fragment destination

--- app/src/main/java/com/brige/newsapp/HomeActivity.java
+++ app/src/main/java/com/brige/newsapp/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.brige.newsapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -27,9 +28,10 @@ public class HomeActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.videoFormFragment)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }

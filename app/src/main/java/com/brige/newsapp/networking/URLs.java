02df8aa2c81ab7a9b3fc9ae894832807bdf11909
Brian <brigeveriz7@gmail.com>
2022-04-05 10:22:00 +0300

    Added a chat service generator

--- app/src/main/java/com/brige/newsapp/networking/URLs.java
+++ app/src/main/java/com/brige/newsapp/networking/URLs.java
@@ -4,5 +4,6 @@ public class URLs {
 
     public static final String BASE_URL = "https://newsapi.org/v2/";
     public static final String API_KEY = "d052b8e50036466587154d2bb262d2eb";
+    public static final String CHAT_URL = "";
 
 }

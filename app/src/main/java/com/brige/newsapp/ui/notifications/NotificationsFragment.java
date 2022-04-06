c62787fe469046e4ac3d5c62915c03a8445e2010
Brian <brigeveriz7@gmail.com>
2022-04-06 10:48:32 +0300

    Added shared preference and auth dialogs

--- app/src/main/java/com/brige/newsapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/brige/newsapp/ui/notifications/NotificationsFragment.java
@@ -1,20 +1,36 @@
 package com.brige.newsapp.ui.notifications;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.brige.newsapp.R;
 import com.brige.newsapp.databinding.FragmentNotificationsBinding;
+import com.brige.newsapp.utils.PreferenceStorage;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private Context context;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = requireActivity();
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +40,68 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        showOrHideLayouts();
+        binding.btnLogin.setOnClickListener(v->{
+            showLoginDialog();
+        });
+
+        binding.btnRegister.setOnClickListener(v -> {
+            showRegisterDialog();
+        });
+
         return root;
     }
+
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void showLoginDialog() {
+
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_login_dialog);
+
+        Button btnLogin = bottomSheetDialog.findViewById(R.id.btn_dialog_login);
+        Button switchRegister = bottomSheetDialog.findViewById(R.id.btn_dialog_switch_register);
+        TextInputEditText inputPass, inputUsername;
+        inputPass = bottomSheetDialog.findViewById(R.id.input_password);
+        inputUsername = bottomSheetDialog.findViewById(R.id.input_username);
+
+
+         btnLogin.setOnClickListener(v ->{
+            new PreferenceStorage(context).setAuthStatus(true);
+            bottomSheetDialog.dismiss();
+            showOrHideLayouts();
+        });
+
+        bottomSheetDialog.show();
+    }
+
+    private void showRegisterDialog() {
+
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_register_dialog);
+    }
+
+    private void showOrHideLayouts() {
+        boolean isAuthenticated = new PreferenceStorage(context).isAuthenticated();
+        if (isAuthenticated){
+            binding.btnRegister.setVisibility(View.GONE);
+            binding.btnLogin.setVisibility(View.GONE);
+            binding.imageView2.setVisibility(View.GONE);
+            binding.textView8.setVisibility(View.GONE);
+            binding.recyclerMessage.setVisibility(View.VISIBLE);
+        }
+        else{
+            binding.btnRegister.setVisibility(View.VISIBLE);
+            binding.btnLogin.setVisibility(View.VISIBLE);
+            binding.imageView2.setVisibility(View.VISIBLE);
+            binding.textView8.setVisibility(View.VISIBLE);
+            binding.recyclerMessage.setVisibility(View.GONE);
+        }
     }
 }
---
+++ app/src/main/java/com/brige/newsapp/utils/PreferenceStorage.java
@@ -0,0 +1,52 @@
+package com.brige.newsapp.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.brige.newsapp.networking.pojos.UserResponse;
+
+public class PreferenceStorage {
+
+    SharedPreferences sharedPreferences;
+    private Context context;
+
+    private final String USER_NAME = "com.brige.newsapp.utils.USER_NAME";
+    private final String USER_EMAIL = "com.brige.newsapp.utils.USER_EMAIL";
+    private final String USER_TOKEN = "com.brige.newsapp.utils.USER_TOKEN";
+    private final String USER_NUMBER = "com.brige.newsapp.utils.USER_NUMBER";
+    private final String SHARED_PREF_NAME = "com.brige.newsapp.utils.SHARED_PREF_NAME";
+    private static final String IS_AUTHENTICATED = "com.brige.newsapp.utils.IS_AUTHENTICATED";
+
+
+    public PreferenceStorage(Context context) {
+        this.sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
+        this.context = context;
+    }
+
+
+    public void setUserData(UserResponse userResponse){
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(USER_NAME, userResponse.getUsername());
+        editor.putString(USER_EMAIL, userResponse.getEmail());
+        editor.putString(USER_NUMBER, userResponse.getPhoneNumber());
+        editor.putString(USER_TOKEN, userResponse.getToken());
+        editor.apply();
+    }
+
+    public String getUserToken(){
+        return sharedPreferences.getString(USER_TOKEN, " ");
+    }
+
+    public boolean isAuthenticated(){
+
+        return sharedPreferences.getBoolean(IS_AUTHENTICATED, false);
+    }
+    public void setAuthStatus(Boolean status){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(IS_AUTHENTICATED, status);
+    }
+
+
+}

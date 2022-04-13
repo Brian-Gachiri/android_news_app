57a831aff21cec185e688b15cc2342ea8e32cd7d
Brian <brigeveriz7@gmail.com>
2022-04-13 10:38:55 +0300

    Added chat details fragment, recyclerview, input layout and chat bubbles layout

--- app/src/main/java/com/brige/newsapp/adapters/ChatAdapter.java
+++ app/src/main/java/com/brige/newsapp/adapters/ChatAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.brige.newsapp.R;
@@ -64,7 +65,9 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             imgChat = itemView.findViewById(R.id.img_chat);
 
             itemView.setOnClickListener(v ->{
-                Snackbar.make(v, "Comming Soon", Snackbar.LENGTH_LONG).show();
+
+                Navigation.findNavController(v).navigate(R.id.chatDetailsFragment);
+//                Snackbar.make(v, "Comming Soon", Snackbar.LENGTH_LONG).show();
             });
         }
     }
